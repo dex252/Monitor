0e84fc97031b795421a93901a50de9ae40ff6c7d
slava <dex252rus@gmail.com>
2025-11-14 21:41:54 +1000

    Управление Background сервисом

--- Monitor/Controllers/HomeController.cs
+++ Monitor/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Monitor.Services;
 
 namespace Monitor.Controllers
 {
@@ -10,17 +11,46 @@ namespace Monitor.Controllers
     [Route("api/[controller]")]
     public class HomeController : ControllerBase
     {
-        private readonly ILogger<HomeController> _logger;
+         private readonly IServiceProvider _serviceProvider;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(IServiceProvider serviceProvider)
         {
-            _logger = logger;
+             _serviceProvider = serviceProvider;
         }
 
         [HttpGet("status")]
         public IActionResult Status()
         {
-            return Ok();
+            var backgroundService = GetBackgroundService();
+            var isSuccess = backgroundService?.Status();
+
+            return Ok(isSuccess);
+        }
+
+        [HttpGet("start")]
+        public async Task<IActionResult> StartService()
+        {
+            var backgroundService = GetBackgroundService();
+            var isSuccess = backgroundService?.Start();
+
+            return Ok(isSuccess);
+        }
+
+        [HttpGet("stop")]
+        public IActionResult StopService()
+        {
+            var backgroundService = GetBackgroundService();
+            var isSuccess = backgroundService?.Stop();
+
+            return Ok(isSuccess);
+        }
+
+         private BackgroundGenerateLogService? GetBackgroundService()
+        {
+            var hostedServices = _serviceProvider.GetServices<IHostedService>();
+            var backgroundService = hostedServices.OfType<BackgroundGenerateLogService>().FirstOrDefault();
+            
+            return backgroundService;
         }
     }
 }
--- Monitor/Services/BackgroundGenerateLogService.cs
+++ Monitor/Services/BackgroundGenerateLogService.cs
@@ -7,32 +7,76 @@ namespace Monitor.Services
 {
     public class BackgroundGenerateLogService : BackgroundService
     {
+        private bool isActive = true;
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
             while (!stoppingToken.IsCancellationRequested)
             {
+
                 try
                 {
+                    if (!isActive)
+                    {
+                        Console.WriteLine($"Процесс остановлен: {isActive}");
+                        continue;
+                    }
                     // Просто ждем 5 секунд и проверяем не запрошена ли отмена
-                    await Task.Delay(5000, stoppingToken);
+
 
                     // Можно добавить лог для демонстрации, что служба жива
-
+                    Console.WriteLine($"Процесс работает: {isActive}");
                 }
                 catch (OperationCanceledException)
                 {
-                    // Это нормально - когда приложение останавливается
-
+                    //приложение останавливается
+                    await Task.Delay(5000, stoppingToken);
                     break;
                 }
-                catch (Exception ex)
+                catch (Exception)
                 {
 
-                    await Task.Delay(10000, stoppingToken); // Ждем 10 сек перед повторной попыткой
+                    await Task.Delay(5000, stoppingToken);
+                }
+                finally
+                {
+                    Console.WriteLine($"FINAL: {isActive}");
+                    await Task.Delay(10000, stoppingToken);
                 }
             }
 
 
+        }
+
+        public override async Task StopAsync(CancellationToken cancellationToken)
+        {
+            await base.StopAsync(cancellationToken);
+        }
+
+        public bool Start()
+        {
+            if (isActive)
+            {
+                return false;
+            }
+
+            isActive = true;
+            return true;
+        }
+
+        public bool Stop()
+        {
+            if (!isActive)
+            {
+                return false;
+            }
+
+            isActive = false;
+            return true;
+        }
+
+        public bool Status()
+        {
+            return isActive;
         }
     }
 }
